4edffd4f5435f8a3168f438a69bb16d4004ea2cf
aussftw <shagronaus@gmail.com>
2019-09-19 17:47:51 +0300

    fist try

--- h/advanced_html_css/hw1/js/script.js
+++ h/advanced_html_css/hw1/js/script.js
@@ -1,6 +1,7 @@
 const sidebar = document.getElementById("navigation__menu");
 const sidebarButton = document.getElementById("navigation__button--cta")
 const icon = document.getElementById("navigation__small-icon")
+const largeIcon = document.getElementById("navigation__large-icon")
 console.log(sidebarButton)
 console.log(sidebar)
 
@@ -9,10 +10,13 @@ sidebarButton.addEventListener('click', () => {
     sidebar.classList.remove('navigation__items-hidden')
     icon.classList.remove('fa-bars')
     icon.classList.add('fa-times')
-    // icon.classList.add('fa-lg')
+    largeIcon.classList.remove("fa-bars")
+    largeIcon.classList.add("fa-times")
   } else {
     sidebar.classList.add('navigation__items-hidden')
     icon.classList.remove('fa-times')
     icon.classList.add("fa-bars")
+    largeIcon.classList.remove('fa-times')
+    largeIcon.classList.add("fa-bars")
   }
 })
