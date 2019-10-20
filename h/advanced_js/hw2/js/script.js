74549953a55eb9fd800a801f7b0915d8eebcfaf0
aussftw <shagronaus@gmail.com>
2019-10-20 19:35:17 +0300

    finished

--- h/advanced_js/hw2/js/script.js
+++ h/advanced_js/hw2/js/script.js
@@ -1,157 +1,141 @@
-
-
-// data
-
-const sizes = [{ val: "small", price: 50, calories: 20 }]
-const stuffings = [
-  { val: "cheese", price: 10, calories: 20 },
-  { val: "salad", price: 20, calories: 5 },
-  { val: "potato", price: 15, calories: 10 }
-]
-
-// Hamburger Class
-
 class Hamburger {
-
-  static SIZE_SMALL = 'small';
-  static SIZE_LARGE = 'large';
-  static STUFFING_CHEESE = 'cheese';
-  static STUFFING_SALAD = 'salad';
-  static STUFFING_POTATO = 'potato';
-  static TOPPING_MAYO = 'mayonese';
-  static TOPPING_SPICE = 'spices';
-
-
-  constructor (size, stuffing) {
-    this.size = size
-    this.stuffing = stuffing
-    this._toppings = []
-  }
-  
-
-  // data validation
-
-  validateInput(el) {
-    for(let key in Hamburger) {
-      if(el == Hamburger[key]) {
-        return true
+  constructor(size, stuffing) {
+    try {
+      if (!size || !stuffing) {
+        throw new HamburgerException("You need to add size or stuffing")
+      } else if (typeof size !== "object" || typeof stuffing !== "object") {
+        throw new HamburgerException("invalid data type")
       }
-    }
-    return false
-  }
 
-  // Mthods
+      this.size = size.size
+      this.stuffing = stuffing.name
 
-  // find size
-
-  get size() {
-    return this._size
-  }
-
-  set size(val) {
-    if (!val) {
-      throw new HamburgerExeption ("Sorry, but you must choose size of your hamburger")
-    } else if (!this.validateInput(val)) {
-      throw new HamburgerExeption(`Sorry, but there invalid "${val}". Please choose correct size`)
-    } else {
-      this._size = val
-    }
-  }
-  
-  // find stuffings 
-
-  get stuffing() {
-    return this._stuffing
-  }
-  set stuffing(val) {
-    if(!val) {
-      throw new HamburgerExeption("Sorry, but you must enter stuffing of your humburger")
-    } else if (!this.validateInput(val)) {
-      throw new HamburgerExeption(`Sorry, but there invalid "${val}". Please choose correct stuffings`)
-    } else {
-      this._stuffing = value
+      const hamSize = ["small", "large"]
+      if (!hamSize.includes(size.size)) {
+        throw new HamburgerException("Invalid size")
+      }
+      const hamStuffings = ["cheese", "salad", "potato"]
+      if (!hamStuffings.includes(stuffing.name)) {
+        throw new HamburgerException("Invalid stuffing")
+      }
+      this.price = size.price + stuffing.price
+      this.calories = size.calories + stuffing.calories
+      this.topping = []
+    } catch {
+      return HamburgerException.message
     }
   }
 
-  // get list of toppings
-
-  get toppings() {
-    return this._toppings
-  }
-
-  // add topping to humburger
-
-  addTopping(item) {
-    if(this.toppings.some(el => el ==  item)) {
-      throw new HamburgerExeption(`You are trying to add ${item}, but you already did it`)
-    } else if (!thisValidateInput(item)) {
-      throw new HamburgerExeption(`Sorry, but ${item} not found.`)
-    } else {
-      this.toppings.push(item)
+  setTopping(topping) {
+    try {
+      const hamToppings = ["mayo", "spice"]
+      if (!hamToppings.includes(topping.name)) {
+        throw new HamburgerException("Invalid topping")
+      } else if (this.topping.find(item => item === topping)) {
+        throw new HamburgerException("You alredy added " + topping.name)
+      } else {
+        this.topping.push(topping)
+        this.price += topping.price
+        this.calories += topping.calories
+      }
+    } catch (e) {
+      return HamburgerException.message
     }
   }
 
-  // remove topping 
-
-  removeTopping(item) {
-    if (this._toppings.some(el => el == item)) {
-      this._toppings = this.toppings.filter(el=>el!==item)
-    } else {
-      throw new HamburgerExeption(`Sorry, but ${item} not found.`)
+  removeTopping(topping) {
+    try {
+      if (this.topping.length > 0) {
+        let toppingIndex = this.topping.find(
+          ingredient => ingredient === topping
+        )
+        let i = this.topping.indexOf(toppingIndex)
+        this.topping.splice(i, 1)
+        this.price -= topping.price
+        this.calories -= topping.calories
+      } else {
+        throw new HamburgerException("no toppings")
+      }
+    } catch (e) {
+      return HamburgerException.message
     }
   }
 
-  //get price
+  getToppings() {
+    return this.topping
+  }
+
+  getSize() {
+    return this.size
+  }
+
+  getStuffing() {
+    return this.stuffing
+  }
 
   calculatePrice() {
-    let price = sizes.find(el => el.value == this._size).price + stuffings.find(el => el.value == this._stuffing).price
-    if (this._toppings[0]) {
-      this._toppings.forEach( function(item) {
-        price += toppings.find(el => el.value == item).price
-      })
-    }
-    return price
+    return this.price
   }
-
-  // get callories
-
   calculateCalories() {
-    let calories = sizes.find(el => el.value == this._size).calories + stuffings.find(el => el.value == this._stuffing).calories
-    if (this._toppings[0]) {
-      this._toppings.forEach( function(item) {
-        calories += toppings.find(el => el.value == item).calories
-      })
-    }
-    return calories
+    return this.calories
   }
-
-  // erros Constructor
-
-  class HamburgerExeption {
-    constructor(message) {
-      this.name = "FFS!"
-      this.message = message
-    }
-
-    get message() {
-      return this._message
-    }
-
-    set message(val) {
-      this._message = val
-    }
-  }
-
-  // testing
-
-  try {
-       
-    } catch (e) {
-        console.error(e.name, e.message);
-    };
-
-
-
-
 }
 
+class HamburgerException extends Error {
+  constructor(message) {
+    super()
+    this.name = "HamburgerException"
+    this.message = message
+    console.log(this.message)
+  }
+}
+
+/* Размеры, виды начинок и добавок */
+Hamburger.SIZE_SMALL = { size: "small", price: 50, calories: 20 }
+Hamburger.SIZE_LARGE = { size: "large", price: 100, calories: 40 }
+Hamburger.STUFFING_CHEESE = { name: "cheese", price: 10, calories: 20 }
+Hamburger.STUFFING_SALAD = { name: "salad", price: 20, calories: 5 }
+Hamburger.STUFFING_POTATO = { name: "potato", price: 15, calories: 10 }
+Hamburger.TOPPING_MAYO = { name: "mayo", price: 20, calories: 5 }
+Hamburger.TOPPING_SPICE = { name: "spice", price: 15, calories: 0 }
+
+// маленький гамбургер с начинкой из сыра
+const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE)
+// добавка из майонеза
+hamburger.setTopping(Hamburger.TOPPING_MAYO)
+// спросим сколько там калорий
+console.log("Calories: %f", hamburger.calculateCalories())
+// сколько стоит
+console.log("Price: %f", hamburger.calculatePrice())
+// я тут передумал и решил добавить еще приправу
+hamburger.setTopping(Hamburger.TOPPING_SPICE)
+// А сколько теперь стоит?
+console.log("Price with spice: %f", hamburger.calculatePrice())
+// Проверить, большой ли гамбургер?
+console.log(
+  "Is hamburger large: %s",
+  hamburger.getSize() === Hamburger.SIZE_LARGE
+) // -> false
+// Убрать добавку
+hamburger.removeTopping(Hamburger.TOPPING_SPICE)
+console.log("Have %d toppings", hamburger.getToppings().length) // 1
+
+const h1 = new Hamburger()
+
+const h2 = new Hamburger(Hamburger.TOPPING_SPICE, Hamburger.TOPPING_SPICE)
+
+const h3 = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.TOPPING_SPICE)
+
+const h4 = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE)
+h4.setTopping(Hamburger.TOPPING_MAYO)
+h4.setTopping(Hamburger.TOPPING_SPICE)
+h4.setTopping(Hamburger.TOPPING_MAYO)
+
+console.log("Hamburger h4 is", h4.getSize())
+console.log("Hamburger h4 has stuffing -", h4.getStuffing())
+console.log(
+  "Hamburger h4 has toppings -",
+  h4.getToppings()[0].name,
+  "and",
+  h4.getToppings()[1].name
+)
