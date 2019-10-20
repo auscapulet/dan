1f6cdbce3f186e2c77aa80e4f514c5a1cda6b0b2
aussftw <shagronaus@gmail.com>
2019-10-20 14:35:43 +0300

    finished

--- h/advanced_js/hw4/js/script.js
+++ h/advanced_js/hw4/js/script.js
@@ -1,31 +1,110 @@
 class Board {
-  constructor(name) {
-    this.name = name
-  }
+  constructor() {}
 
   render() {
     const boardColumn = document.createElement("div")
     boardColumn.classList.add("boardColumn")
     document.getElementsByTagName("script")[0].before(boardColumn)
 
+    function allowDrop(e) {
+      e.preventDefault()
+    }
+
+    function drop(e) {
+      e.preventDefault()
+      const data = e.dataTransfer.getData("text")
+      this.appendChild(document.getElementById(data))
+      e.dataTransfer.clearData()
+    }
+
+    const table = document.getElementsByClassName("boardColumn")
+    for (let i = 0; i < table.length; i++) {
+      table[i].addEventListener("dragover", allowDrop)
+      table[i].addEventListener("drop", drop)
+    }
+
     const buttonCardConstructor = document.createElement("button")
     buttonCardConstructor.classList.add("buttonCardConstructor")
+    buttonCardConstructor.setAttribute("id", "openModal")
     buttonCardConstructor.innerText = "Create card"
     boardColumn.append(buttonCardConstructor)
 
     buttonCardConstructor.addEventListener("click", e => {
-      const cardText = document.createElement("input")
-      cardText.classList.add("cardText")
-      buttonCardConstructor.append(cardText)
+      const modalContainer = document.createElement("div")
+      modalContainer.classList.add("modal")
+      document.getElementsByTagName("script")[0].before(modalContainer)
 
-      cardText.addEventListener("keyup", e => {
-        const cardDataContainer = document.createElement("div")
-        cardDataContainer.classList.add("cardData")
-        boardColumn.append(cardDataContainer)
+      const modalContent = document.createElement("div")
+      modalContent.classList.add("modal-content")
+      modalContainer.append(modalContent)
 
-        const cardInfo = document.createElement("p")
-        cardInfo.innerText = cardText.value
-        cardDataContainer.append(cardInfo)
+      const closeModal = document.createElement("span")
+      closeModal.classList.add("modal-close")
+      closeModal.innerHTML = "&times;"
+      modalContent.append(closeModal)
+
+      const modalInput = document.createElement("input")
+      modalInput.classList.add("cardText")
+      modalContent.append(modalInput)
+
+      const confirmButton = document.createElement("button")
+      confirmButton.classList.add("confirmButton")
+      confirmButton.innerText = "OK"
+      modalContent.append(confirmButton)
+
+      modalInput.addEventListener("keyup", e => {
+        const modalInputValue = e.target.value
+        return modalInputValue
+      })
+
+      // console.log(modalInputValue)
+
+      confirmButton.addEventListener("click", e => {
+        const card = document.createElement("div")
+
+        const id = (function() {
+          return (
+            "card-id-" +
+            Math.random()
+              .toString(36)
+              .substr(2, 3)
+          )
+        })()
+
+        card.setAttribute("id", `${id}`)
+        card.setAttribute("draggable", "true")
+        card.classList.add("card")
+        modalContainer.style.display = "none"
+        boardColumn.append(card)
+
+        const cardText = document.createElement("p")
+        cardText.innerText = modalInputValue
+        card.append(cardText)
+      })
+
+      closeModal.addEventListener("click", e => {
+        modalContainer.style.display = "none"
+      })
+
+      function allowDrop(e) {
+        e.preventDefault()
+      }
+
+      function drag(e) {
+        e.dataTransfer.setData("text", e.target.vlaue)
+      }
+
+      function drop(e) {
+        e.preventDefault()
+        const data = e.dataTransfer.getData("text")
+        this.appendChild(document.getElementById(data))
+      }
+
+      // get all cards
+
+      const cards = document.querySelectorAll(".card")
+      cards.forEach(item => {
+        item.addEventListener("dragstart", drag)
       })
     })
   }
--- h/advanced_js/hw6/js/script.js
+++ h/advanced_js/hw6/js/script.js
@@ -1,65 +1,27 @@
-function Hamburger(size, stuffing) {
-    /**
-  * Класс, объекты которого описывают параметры гамбургера. 
-  * 
-  * @constructor
-  * @param size        Размер
-  * @param stuffing    Начинка
-  * @throws {HamburgerException}  При неправильном использовании
-  */
-  
-  /* Размеры, виды начинок и добавок */
+const planetsURLS = [
+  "https://swapi.co/api/planets/",
+  "https://swapi.co/api/planets/2",
+  "https://swapi.co/api/planets/3",
+  "https://swapi.co/api/planets/4",
+  "https://swapi.co/api/planets/5",
+  "https://swapi.co/api/planets/6",
+  "https://swapi.co/api/planets/7"
+]
 
-  Hamburger.SIZE_SMALL = "size_small";
-  Hamburger.SIZE_LARGE = "size_large";
-  Hamburger.STUFFING_CHEESE = "stuffing_cheese";
-  Hamburger.STUFFING_SALAD = "stuffing_salad";
-  Hamburger.STUFFING_POTATO = "stuffing_potato";
-  Hamburger.TOPPING_MAYO = "topping_mayo";
-  Hamburger.TOPPING_SPICE = "topping_spice";
+Promise.all(planetsURLS.map(item => fetch(item)))
+  .then(responses => Promise.all(responses.map(res => res.json())))
+  .then(texts => {
+    console.log(texts)
 
-  // collecting data
-
-  var sizes = {};
-  var stuffings = {};
-  var toppings = {};
-
-  sizes[Hamburger.SIZE_SMALL] = {price: 50, calories: 20};
-  sizes[Hamburger.SIZE_LARGE] = {price: 100, calories: 40};
-
-  stuffings[Hamburger.STUFFING_CHEESE] = {price: 10, calories: 20};
-  stuffings[Hamburger.STUFFING_SALAD] = {price: 20, calories: 5};
-  stuffings[Hamburger.STUFFING_POTATO] ={price: 15, calories: 10};
-
-  toppings[Hamburger.TOPPING_MAYO] = {price: 20, calories: 20};
-  toppings[Hamburger.TOPPING_SPICE] = {price: 15, calories: 5}
-
-  // constructor
-
-  this.size = size;
-  this.stuffing = stuffing;
-  this._toppings = [];
-
-  // validation input data
-
-  function inputValidation(el) {
-    for(let key in Hamburger) {
-      if(el === Hamburger[key]) {
-        return  true;
-      }
+    for (let i = 0; i < texts.length; i++) {
+      console.log(i.name)
     }
-    return false;
-  }
 
-  if (!size || !stuffing) {
-    throw new HamburgerException (`Sorry, but you must choose both - size and stuffing for`)
-  };
-  if(!inputValidation(size)) {
-    throw new HamburgerException(`Sorry, but there invalid "${size}". Please choose correct size`)
-  };
-  if(!inputValidation(stuffing)) {
-    throw new HamburgerException(`Sorry, but there invalid "${stuffing}".Please choose correct size`)
-  }
+    // let obj = texts.find(i => i.name === "name")
+    // console.log(obj)
+    // let filter = texts.filter(i => i.name === name)
+    // console.log(filter)
 
-
-}
+    // let some = texts.filter(i => i.name === name)
+    // console.log(some)
+  })
